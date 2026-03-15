889d1b81427347503df339cb15c457698b6c67b9
dhilipan414 <dhilipan17082000@hotmail.com>
2026-03-15 11:55:02 +0530

    Backend Deployment Files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,12 +70,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # ─── DATABASE ────────────────────────────────────────────────
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600,
-        )
-    }
+    DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
 else:
     DATABASES = {
         'default': {
